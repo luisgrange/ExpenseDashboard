be7543585b243646bef0794bdd238a8f21c287e1
luisgrange <luisfernando237grange@outlook.com>
2025-03-11 09:34:14 -0300

    Removing token in register process

--- ExpensesDashboard/src/main/java/ExpensesDashboard/Application/DTOs/RegisterRequestDto.java
+++ ExpensesDashboard/src/main/java/ExpensesDashboard/Application/DTOs/RegisterRequestDto.java
@@ -1,4 +1,4 @@
 package ExpensesDashboard.Application.DTOs;
 
-public record RegisterRequestDto(String name, String email, String role,String password, Double salary) {
+public record RegisterRequestDto(String name, String email, String password, Double salary) {
 }
